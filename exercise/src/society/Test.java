7b6445c5511ed9a016fd2bfb9a169cc22802bfe2
wanzifa <wanzifun@gmail.com>
2016-04-12 10:36:36 +0800

    add exercise

---
+++ exercise/src/society/Test.java
@@ -0,0 +1,11 @@
+package society;
+import society.Human;
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Human aPerson = new Human(180);
+        System.out.println(aPerson.getHeight());
+    }
+
+}
